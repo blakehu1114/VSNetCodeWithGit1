84dc6316ea24536d77b9291c200366b6bb4c5649
Blake Hu <blakehu7@gmail.com>
2024-11-08 10:53:11 +0800

    Remove .vs folder from repository

--- CodeWithGit1/obj/Debug/net8.0/CodeWithGit1.AssemblyInfo.cs
+++ CodeWithGit1/obj/Debug/net8.0/CodeWithGit1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodeWithGit1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69bc861251053f222239ef2f1f3de9689da0c6fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodeWithGit1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodeWithGit1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
